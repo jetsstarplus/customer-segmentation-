706006f8b9643dda0c9aceedf731d4ded33590b7
unknown <jets.starplus@gmail.com>
2020-10-19 17:33:13 +0300

    project y3

--- analytics/views.py
+++ analytics/views.py
@@ -7,9 +7,10 @@ from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request, 'analytics/home.html')
 
-
+# http://data.world/victorotieno
 @login_required(login_url = 'login')
 def index(request):
+
     return render(request,'analytics/dashboard-2.html')
 
 
